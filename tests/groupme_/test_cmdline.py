764c7ddbeecc2c54effd39a92bebfffd2252a5c9
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-04-13 11:53:15 +0200

    test(groupme): add test to install and run groupme

---
+++ tests/groupme_/test_cmdline.py
@@ -0,0 +1,22 @@
+#==============================================================================
+# C O P Y R I G H T
+#------------------------------------------------------------------------------
+# Copyright (c) 2019 by Helmut Konrad Fahrendholz. All rights reserved.
+# Tis file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+#==============================================================================
+
+from groupme import ROOT
+from utila.test import run
+from utila.test import skip_not_virtual
+
+
+@skip_not_virtual
+def test_run_groupme():
+    uninstall = 'pip uninstall groupme'
+    install = 'python setup.py install && groupme --help'
+
+    clean_and_run = uninstall + ' && ' + install
+    completed = run(clean_and_run, cwd=ROOT)
+    assert completed.returncode == 0, completed.stdout + completed.stderr
