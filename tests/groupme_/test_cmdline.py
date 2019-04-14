97ad07d229dc000e7fd878dc0b86abbc2aa7056e
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-04-14 10:38:21 +0200

    fix(test): remove package before test installation

--- tests/groupme_/test_cmdline.py
+++ tests/groupme_/test_cmdline.py
@@ -1,20 +1,22 @@
-#==============================================================================
+# =============================================================================
 # C O P Y R I G H T
-#------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Copyright (c) 2019 by Helmut Konrad Fahrendholz. All rights reserved.
 # Tis file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
 # use or distribution is an offensive act against international law and may
 # be prosecuted under federal law. Its content is company confidential.
-#==============================================================================
+# =============================================================================
 
-from groupme import ROOT
 from utila.test import run
 from utila.test import skip_not_virtual
+
+from groupme import ROOT
 
 
 @skip_not_virtual
 def test_run_groupme():
-    uninstall = 'pip uninstall groupme'
+    """Install groupme and run groupme --help to ensure basic function"""
+    uninstall = 'pip uninstall hey -y'
     install = 'python setup.py install && groupme --help'
 
     clean_and_run = uninstall + ' && ' + install
