7614fea35ace38986474785a82e72ee71be30e16
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-06-14 17:50:29 +0200

    test(resources): make invocation location from script independent

--- tests/groupme_/data/update.sh
+++ tests/groupme_/data/update.sh
@@ -1,5 +1,10 @@
 #! /usr/bin/bash
 
+scriptpath=$(readlink -f "$0")
+scriptroot=$(dirname "$scriptpath")
+
+pushd $scriptroot
+
 # footer
 
 rawmaker -i footer/restructuredtext.pdf -o ./footer --prefix=oneline --text --char_margin=100.0
@@ -9,3 +14,5 @@ rawmaker -i footer/restructuredtext.pdf -o ./footer. --toc --text --char_margin=
 # simple
 
 rawmaker -i simple/howto_pyporting.pdf -o ./simple --toc --text --font
+
+popd
