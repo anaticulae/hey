dd7424e93b7860e6e726639246ff048e18db4642
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-06-14 20:15:33 +0200

    feat(sections): create workplan with toc, index and title
    Add description what sections tools does.

--- sections/command/__init__.py
+++ sections/command/__init__.py
@@ -9,22 +9,46 @@
 
 from os.path import join
 
+from utila import create_step
 from utila import featurepack
 
 from sections import PROCESS_NAME
 from sections import ROOT
 from sections import __version__
+from sections.feature.index import work as work_index
+from sections.feature.title import work as work_title
+from sections.feature.toc import work as work_toc
 
-DESCRIPTION = 'TODO'
+DESCRIPTION = ('The sections tool analyses every single page of an pdf file '
+               'and determines the likelihood to be an feature')
+
 WORKPLAN = [
-    # create_step(
-    #     PROCESS_NAME,
-    #     extract_chapter,
-    #     inputs=[
-    #         ('rawmaker', 'text_text'),
-    #     ],
-    #     output=('chapter',),
-    # ),
+    create_step(
+        PROCESS_NAME,
+        work_index,
+        inputs=[
+            ('rawmaker', 'oneline_text_text'),
+        ],
+        output=('likelihood_index',),
+    ),
+    create_step(
+        PROCESS_NAME,
+        work_toc,
+        inputs=[
+            ('rawmaker', 'oneline_text_text'),
+        ],
+        output=('likelihood_toc',),
+    ),
+    create_step(
+        PROCESS_NAME,
+        work_title,
+        inputs=[
+            ('rawmaker', 'text_text'),
+            ('rawmaker', 'fonts_header'),
+            ('rawmaker', 'fonts_content'),
+        ],
+        output=('likelihood_title',),
+    ),
 ]
 
 
