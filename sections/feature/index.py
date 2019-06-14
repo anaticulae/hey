acab70e7f7c9560bc2f6d883bc1153e96b1834fb
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-06-14 14:31:30 +0200

    docs(index): extend interface document of `index` feature

--- sections/feature/index.py
+++ sections/feature/index.py
@@ -11,43 +11,76 @@ from re import X
 from re import compile as recompile
 from re import match
 from typing import List
+from typing import Tuple
 
 from iamraw import Document
+from iamraw import Page
 from serializeraw import load_document
 
 from sections.feature import uniform_result
 
 
-def work(text_linewise: str, pagenumbers: str) -> str:
+def work(text_linewise: str) -> str:
+    """Load document and extract likelihood of beening an index page
+
+    Args:
+        text_linewise(str): path to document with high `char_margin`
+    Returns:
+        yaml content with dumped result for every single page
+    """
     # TODO: Share resources
     document = load_document(text_linewise)
     extract_index_likelihood(document)
 
 
 def extract_index_likelihood(document: Document) -> List[float]:
+    """Extract likelihood of beeing an index page. Determine a likelihood for
+    every single page.
+
+    Args:
+        document(Document): document to iterate over single pages
+    Returns:
+        a normalized List[float] with the normalized likelihood of beeing an
+        index page
+    """
     result = [analyse_page(page) for page in document]
     uniformed = uniform_result(result)
+    assert len(uniformed) == len(document)
     return uniformed
 
 
-def analyse_page(page) -> float:
+# INDEX, PAGENUMBER
+INDEX_ITEM_PATTERN = recompile(
+    r"""^([a-zA-Z]+\s?){1,3} # one till three words
+          [\s|,]?            # optional `,`
+          \s{0,5}            # between zero and five spaces
+          [0-9]+$            # a pagenumber at the end
+     """, X)
+
+
+def analyse_page(page: Page) -> Tuple[int, int]:
+    """Extract potential features of an index page
+
+    This methods search for 2 features. The simpelst feature is a single
+    char. This char represents the index A-Z. The second feature is a pattern
+    out of index-name and index-page.
+
+    Args:
+        page(Page): page to search for index features
+    Returns:
+        (linecount, matched features):
+    """
     content = page.text.splitlines()
     linecount = len(content)
 
+    # search for single chars, which represents the index
     single_char = [
         item for item in content if len(item) == 1 and item.isalpha()
     ]
 
-    # INDEX, PAGENUMBER
-    pattern = recompile(
-        r"""^([a-zA-Z]+\s?){1,3}
-                    [\s|,]?
-                    \s{0,5}
-                    [0-9]+$
-                """, X)
-    index_with_page = [line for line in content if match(pattern, line)]
-    for item in index_with_page:
-        print(item)
+    index_with_page = [
+        line for line in content if match(INDEX_ITEM_PATTERN, line)
+    ]
     single_char_or_index_with_page = len(single_char) + len(index_with_page)
 
     return linecount, single_char_or_index_with_page
