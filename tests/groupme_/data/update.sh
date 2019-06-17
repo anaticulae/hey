4934baca26b340dd4fbc480a82f308367b1a4ed0
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-06-17 08:25:24 +0200

    test(refactor): format update script

--- tests/groupme_/data/update.sh
+++ tests/groupme_/data/update.sh
@@ -7,12 +7,27 @@ pushd $scriptroot
 
 # footer
 
-rawmaker -i footer/restructuredtext.pdf -o ./footer --prefix=oneline --text --char_margin=100.0
+rawmaker -i footer/restructuredtext.pdf\
+         -o ./footer\
+         --prefix=oneline\
+         --text\
+         --char_margin=100.0
 
-rawmaker -i footer/restructuredtext.pdf -o ./footer. --toc --text --char_margin=5.0 --border --boxes --font
+rawmaker -i footer/restructuredtext.pdf\
+         -o ./footer\
+         --border\
+         --boxes\
+         --font\
+         --text\
+         --char_margin=5.0\
+         --toc
 
 # simple
 
-rawmaker -i simple/howto_pyporting.pdf -o ./simple --toc --text --font
+rawmaker -i simple/howto_pyporting.pdf\
+         -o ./simple\
+         --font\
+         --text\
+         --toc
 
 popd
