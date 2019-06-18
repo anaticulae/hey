0d746890fdcb473d069c149ec38f1b38c0548eab
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-06-18 07:55:02 +0200

    refactor(test): move PageTextNavigator example to general package
    Reuse more test code.

--- tests/groupme_/__init__.py
+++ tests/groupme_/__init__.py
@@ -9,10 +9,13 @@
 
 from functools import partial
 
+from iamraw import BoundingBox
+from pytest import fixture
 from utila import run_command
 
 from groupme.command import PROCESS_NAME
 from groupme.command import main
+from groupme.textnavigator.navigator import PageTextNavigator
 
 #pylint: disable=invalid-name
 run_success = partial(
@@ -28,3 +31,24 @@ run_failure = partial(
     process=PROCESS_NAME,
     success=False,
 )
+
+SAMPLE = [
+    (0, BoundingBox.from_str("130.91 668.55 540.00 704.02")),
+    (2, BoundingBox.from_str("358.45 605.24 480.47 625.77")),
+    (1, BoundingBox.from_str("467.46 650.40 540.00 667.51")),
+    (4, BoundingBox.from_str("409.67 513.88 540.01 558.02")),
+    (5, BoundingBox.from_str("550.0 513.88 600.0 558.02")),
+    (3, BoundingBox.from_str("304.91 587.31 534.01 607.84")),
+    (6, BoundingBox.from_str("77.38 216.25 121.22 230.47")),
+    (8, BoundingBox.from_str("303.26 40.18 308.74 54.44")),
+    (7, BoundingBox.from_str("77.38 102.67 534.62 206.45")),
+]
+
+
+@fixture
+def navigator() -> PageTextNavigator:
+    result = PageTextNavigator()
+    for item, position in SAMPLE:
+        result.insert(position, item)
+    assert len(result) == len(SAMPLE)
+    return result
--- tests/groupme_/test_textnavigator.py
+++ tests/groupme_/test_textnavigator.py
@@ -13,29 +13,8 @@ from pytest import mark
 from groupme.textnavigator import PageTextNavigator
 from groupme.textnavigator import percent_to_pagesize
 
-SAMPLE = [
-    (0, BoundingBox.from_str("130.91 668.55 540.00 704.02")),
-    (2, BoundingBox.from_str("358.45 605.24 480.47 625.77")),
-    (1, BoundingBox.from_str("467.46 650.40 540.00 667.51")),
-    (4, BoundingBox.from_str("409.67 513.88 540.01 558.02")),
-    (5, BoundingBox.from_str("550.0 513.88 600.0 558.02")),
-    (3, BoundingBox.from_str("304.91 587.31 534.01 607.84")),
-    (6, BoundingBox.from_str("77.38 216.25 121.22 230.47")),
-    (8, BoundingBox.from_str("303.26 40.18 308.74 54.44")),
-    (7, BoundingBox.from_str("77.38 102.67 534.62 206.45")),
-]
 
-
-@fixture
-def navigator():
-    result = PageTextNavigator()
-    for item, position in SAMPLE:
-        result.insert(position, item)
-    assert len(result) == len(SAMPLE)
-    return result
-
-
-def test_insert_order(navigator):  #pylint:disable=W0621
+def test_insert_order(navigator: PageTextNavigator):  #pylint:disable=W0621
     for index in range(len(navigator) - 1):
         before, _ = navigator[index]
         after, _ = navigator[index + 1]
