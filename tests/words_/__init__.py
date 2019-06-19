d58d88ad4f24ecd1119e22bb0eed30c371a2dff4
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-06-19 18:25:34 +0200

    docs(words): add required resource in words description

--- tests/words_/__init__.py
+++ tests/words_/__init__.py
@@ -6,7 +6,13 @@
 # use or distribution is an offensive act against international law and may
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
-
+"""
+    Required resources:
+        * text
+        * font
+        * position
+        * page-size, to determine the distance from left border to text
+"""
 from functools import partial
 
 from utila import run_command
