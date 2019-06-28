f64c5e82362d15d3f9fa09230e5088b29f59e8d5
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-06-28 20:52:18 +0200

    feat(feature): add templates for new features style, text, words

---
+++ words/feature/text.py
@@ -0,0 +1,31 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2019 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+"""
+
+for chapter in content:
+    print(chapter.title)
+    for paragraph in chapter:
+        print(paragraph.title)
+        print(paragraph.number)
+        for sentence in paragraph:
+            print('word cout: %d' % len(sentence))
+            for word in sentence:
+                print(word)
+
+word
+
+word.font
+word.font.color
+word.font.size
+word.style = [i, b, u, strong? etc?]
+"""
+
+
+def work():
+    pass
---
+++ words/feature/words.py
@@ -0,0 +1,28 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2019 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+"""
+<document>
+<h1>Chapter 1</h1>
+<p>
+Hallo <b>bold</b>,
+
+wie geht es ihnen?
+
+<i>Danke</i> sehr <u>gut</u>
+</p>
+<h1>Chapter 2</h1>
+
+<h1>Chapter 3</h1>
+</document>
+
+"""
+
+
+def work():
+    pass
