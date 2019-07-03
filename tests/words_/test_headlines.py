37299df0735a0930c6004ee5a82e8c68f981e68b
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-07-03 17:36:53 +0200

    test(headlines): add test for headlines work-method

--- tests/words_/test_headlines.py
+++ tests/words_/test_headlines.py
@@ -7,6 +7,12 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 from hey.textnavigator.navigator import create_pagetextnavigator
+from sections.feature.chapter import work as chapter_work
+from sections.feature.index import work as index_work
+from sections.feature.sections import work as section_work
+from sections.feature.title import work as title_work
+from sections.feature.toc import work as toc_work
+from sections.feature.whitepage import work as whitepage_work
 # pylint:disable=W0611
 from tests.fixtures.restruct import restructured
 from tests.fixtures.restruct import restructured_document
@@ -15,10 +21,18 @@ from tests.fixtures.restruct import restructured_horizontals
 from tests.fixtures.restruct import restructured_sections
 from tests.fixtures.restruct import restructured_sizeandborder
 from tests.fixtures.restruct import restructured_text_positions
+from tests.resources import RESTRUCT_FONT_CONTENT
+from tests.resources import RESTRUCT_FONT_HEADER
+from tests.resources import RESTRUCT_HORIZONTAL
+from tests.resources import RESTRUCT_PAGESIZE
+from tests.resources import RESTRUCT_TEXT
+from tests.resources import RESTRUCT_TEXT_POSITION
+from tests.resources import RESTRUCT_TOC
 from words.feature.headlines import Headline
 from words.feature.headlines import dump_headlines
 from words.feature.headlines import extract_headlines
 from words.feature.headlines import load_headlines
+from words.feature.headlines import work
 
 EXPECTED = [
     [
@@ -94,8 +108,43 @@ def test_headlines_extract_headlines(
         horizontals=restructured_horizontals,
         chapter=[0, 1],
     )
-
+    assert len(extracted) == len(EXPECTED)
+    assert [len(item) for item in extracted] == [len(item) for item in EXPECTED]
     assert extracted == EXPECTED
+
+
+def sections():
+    chapter = chapter_work(RESTRUCT_TEXT, RESTRUCT_TEXT_POSITION, RESTRUCT_TOC)
+
+    index = index_work(RESTRUCT_TEXT)
+    title = title_work(
+        RESTRUCT_TEXT,
+        RESTRUCT_FONT_HEADER,
+        RESTRUCT_FONT_CONTENT,
+    )
+    toc = toc_work(RESTRUCT_TEXT)
+    whitepage = whitepage_work(
+        RESTRUCT_TEXT,
+        RESTRUCT_TEXT_POSITION,
+        RESTRUCT_HORIZONTAL,
+    )
+    result = section_work(chapter, index, title, toc, whitepage)
+    return result
+
+
+def test_headlines_work():
+    sections_ = sections()
+    dumped = work(
+        sections=sections_,
+        text=RESTRUCT_TEXT,
+        text_position=RESTRUCT_TEXT_POSITION,
+        font_header=RESTRUCT_FONT_HEADER,
+        font_content=RESTRUCT_FONT_CONTENT,
+        sizeandborder=RESTRUCT_PAGESIZE,
+        horizontals=RESTRUCT_HORIZONTAL,
+    )
+    # dump some headlines
+    assert len(dumped) > 2100, str(dumped)
 
 
 def test_headlines_dump_and_load_headlines():
