9243393d62e8d418d88f913cfb05436029007a52
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-07-06 18:03:00 +0200

    test(text): add test to verify headlines and content on special pages

--- tests/words_/test_text.py
+++ tests/words_/test_text.py
@@ -8,6 +8,9 @@
 # =============================================================================
 
 from pytest import fixture
+from pytest import mark
+from pytest import param
+from utila import NEWLINE
 
 from tests.resources import RESTRUCT_BOXES
 from tests.resources import RESTRUCT_FONT_CONTENT
@@ -19,8 +22,10 @@ from tests.resources import RESTRUCT_TEXT_POSITION
 # pylint:disable=W0611
 from tests.words_.test_headlines import restructured_headlines
 from words.feature.headlines import load_headlines
+from words.feature.text import analyze_page
 from words.feature.text import dump_text
 from words.feature.text import extract_texts
+from words.feature.text import fill_headlines
 from words.feature.text import load_text
 from words.feature.text import prepare_input
 from words.feature.text import work
@@ -127,3 +132,89 @@ def test_words_text_extractor_titles(textexample):
     #page18
     assert result[10][1][0][0].text == 'CHAPTER 5'
     assert result[10][1][1][0].text == 'Sphinx Guide'
+
+
+@mark.parametrize('current_page,current_headline,expected_start,expected_end', [
+    param(
+        13,
+        7,
+        ('Getting Started'),
+        ('make html is the main way you will '
+         'build HTML documentation locally. It is simply a wrapper '
+         'around a more complex call to Sphinx.'),
+        id='page 13',
+    ),
+    param(
+        14,
+        8,
+        ('Now that we have our basic skeleton, let’s document the project. '
+         'As you might have guessed from the name, we’ll be documenting a'
+         ' basic web crawler.'),
+        ('Include the following in your install.rst:'),
+        id='page 14',
+    ),
+    param(
+        15,
+        9,
+        ('u0'),
+        (None),
+        id='page 15',
+    ),
+    param(
+        16,
+        10,
+        ('Make a manpage'),
+        ('u21'),
+        id='page 16',
+    ),
+])
+def test_words_extract_texts_page_x(
+        current_page,
+        current_headline,
+        expected_start,
+        expected_end,
+        restructured_headlines,
+):
+    headlines = restructured_headlines
+    border, fontstore, headlines, textnavigators, boxes = prepare_input(
+        text=RESTRUCT_TEXT,
+        text_position=RESTRUCT_TEXT_POSITION,
+        font_header=RESTRUCT_FONT_HEADER,
+        font_content=RESTRUCT_FONT_CONTENT,
+        headlines=headlines,
+        pagesizes=RESTRUCT_PAGESIZE,
+        horizontals=RESTRUCT_HORIZONTAL,
+        boxes=RESTRUCT_BOXES,
+    )
+
+    # fill headlines
+    headlines = fill_headlines(headlines)
+    # ensure that all collect headlines are from page 13
+    current = headlines[current_headline]
+    assert all([item.page == current_page for item in current])
+
+    analyzed = analyze_page(current, fontstore, textnavigators, border, boxes)
+
+    page, (content) = analyzed
+    assert page == current_page, 'wrong extracted page: %d' % page
+    first_headline, first_output = content[0][0], content[0][1]
+    last_headline, last_output = content[-1][0], content[-1][1]
+    # msg = 'invalid returned value due `analyze_page`'
+    # assert headline.text == current[0].text, '%s\n%s' % (msg, output)
+    assert first_headline.page == current_page
+    assert last_headline.page == current_page
+
+    assert first_output, str(first_output)
+
+    first_line = join_output(first_output[0])
+    last_line = join_output(last_output[-1]) if last_output else None
+    assert last_line == expected_end
+    assert first_line == expected_start
+
+
+def join_output(paragraph):
+    try:
+        joined = ''.join([item for (item, _) in paragraph.content])
+        return joined.replace(NEWLINE, ' ')
+    except TypeError:
+        return 'u%d' % paragraph.container
