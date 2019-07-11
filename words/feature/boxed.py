3535f1d95201775e0f1664857e3831e21562e2f5
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-07-11 19:51:03 +0200

    feat(boxed): add feature to extract boxes from document
    + add method to dump and load boxed

---
+++ words/feature/boxed.py
@@ -0,0 +1,193 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2019 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+"""
+TODO: Think about this complex data structure. Do we need this realy?
+"""
+from collections import defaultdict
+from functools import lru_cache
+from functools import partial
+
+from iamraw import BoundingBox
+from serializeraw import load_boxes
+from utila import checkdatatype
+from utila import from_raw_or_path
+from yaml import FullLoader
+from yaml import dump
+from yaml import load
+
+from hey import SMALL_CACHE
+from words.boxed import NO_BOX
+from words.boxed import BoxedChecker
+from words.input import prepare_input
+from words.input import process_input
+
+
+@checkdatatype
+def work(
+        extracted_text: str,
+        text: str,
+        text_position: str,
+        headlines: str,
+        border: str,
+        horizontals: str,
+        boxes: str,
+) -> str:
+    """Combine `extracted_text` and check the `undefined` fields for lists
+
+    Args:
+        extracted_text(str): document with `undefined fields` from `text`
+                             module of `words`
+        text(str): extracted text from rawmaker
+        text_position(str): position of extracted text
+        headlines(str): extracted chapter/paragraph headlines of `words` module
+        border(str):
+    """
+    extracted, _ = prepare_input(
+        extracted_text,
+        text,
+        text_position,
+        border,
+        headlines,
+        horizontals,
+    )
+    boxes = load_boxes(boxes)
+    result = process_content(extracted, boxes)
+    dumped = dump_boxedcontent(result)
+    return dumped
+
+
+def process_content(extracted, boxes):
+    boxes = BoxedChecker(boxes)
+    worker = partial(extract_boxed_content, boxed=boxes)
+    result = process_input(extracted, worker)
+    return result
+
+
+def extract_boxed_content(contentblock, boxed: BoxedChecker):
+    result = defaultdict(list)
+    for (page, headlinenumber, headlinecontent) in contentblock:
+
+        for index, (headlineblocknumber, blocks) in enumerate(headlinecontent):
+
+            collected = []
+            current = defaultdict(list)
+            for (bounding, line) in blocks:
+                boxid = boxed.boxid(page, bounding)
+                if boxid == NO_BOX:
+                    # splitted by non-box-element
+                    if not current:
+                        continue
+                    collected.append([(
+                        boxed.boundingbox(page, boxid_),
+                        (boxid_, content),
+                    ) for boxid_, content in current.items()])
+                    current = defaultdict(list)
+                # add line to box, defined by `boxid`
+                current[boxid].append((bounding, line))
+            # item ends with box
+            if current:
+                collected.append([(
+                    boxed.boundingbox(page, item),
+                    (item, content),
+                ) for item, content in current.items()])
+
+            if collected:
+                result[page].append((
+                    headlinenumber,
+                    headlineblocknumber,
+                    collected,
+                ))
+
+    if not result:
+        return None
+    assert len(result) == 1, len(result)
+    for key, value in result.items():
+        return (key, value)
+
+
+def dump_boxedcontent(boxed) -> str:
+
+    # headlinenumber,
+    # headlineblocknumber,
+    # collected,
+
+    # BoundingBox
+    # boxid, content
+    raw = []
+    for (page, pagecontent) in boxed:
+        pageresult = []
+        for (headlinenumber, headlineblocknumber, collected) in pagecontent:
+            # for (bounding, blockcontent) in collected:
+            # more than one box in a box-container:
+            # content, box, box, content, box, content
+            single_collector = []  # crazy naming!
+            for multiboxed in collected:
+                items = []
+                for index, item in enumerate(multiboxed):
+                    bounding, (boxid, _content) = item
+                    items.append({
+                        'boxed_id':
+                        '%d %d' % (boxid, index),
+                        'bounding':
+                        str(bounding),
+                        'content': [
+                            '%s %s' % (str(bounding), contentitem)
+                            for (bounding, contentitem) in _content
+                        ]
+                    })
+                single_collector.append(items)
+            pageresult.append({
+                'headlinenumber': headlinenumber,
+                'headlineblocknumber': headlineblocknumber,
+                'content': single_collector,
+            })
+
+        raw.append({
+            'page': page,
+            'content': pageresult,
+        })
+
+    dumped = dump(raw)
+    return dumped
+
+
+@lru_cache(SMALL_CACHE)
+def load_boxedcontent(content: str):
+
+    def _parse_box_content(line: str):
+        splitted = line.split(maxsplit=4)
+        bounding = BoundingBox.from_str(' '.join(splitted[0:4]))
+        return (bounding, splitted[4])
+
+    content = from_raw_or_path(content, ftype='yaml')
+    loaded = load(content, Loader=FullLoader)
+    pagedict = defaultdict(list)
+    for line in loaded:
+        page, __content = line['page'], line['content']
+        for item in __content:
+            multiboxed = []
+            headlinenumber = item['headlinenumber']
+            headlineblocknumber = item['headlineblocknumber']
+            for single_collector in item['content']:
+                boxed = []
+                for multibox in single_collector:
+                    m_bounding = BoundingBox.from_str(multibox['bounding'])
+                    m_content = multibox['content']
+                    boxid, _ = [  # boxid, index
+                        int(item) for item in multibox['boxed_id'].split()
+                    ]
+                    m_content = [_parse_box_content(item) for item in m_content]
+                    boxed.append((m_bounding, (boxid, m_content)))
+                multiboxed.append(boxed)
+            pagedict[page].append((headlinenumber, headlineblocknumber,
+                                   multiboxed))
+    result = []
+    for page, value in pagedict.items():
+        result.append((page, value))
+    return result
