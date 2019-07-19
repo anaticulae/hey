67c974c5560c618046d8ca496dd8ce176c05a8f1
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-07-19 07:41:52 +0200

    1.2.4 automatically generated release

--- hey/__init__.py
+++ hey/__init__.py
@@ -8,7 +8,7 @@
 #==============================================================================
 import os
 
-__version__ = '1.2.3'
+__version__ = '1.2.4'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
