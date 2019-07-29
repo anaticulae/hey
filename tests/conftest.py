6c880f481c5fb6c30040ebc3daf0a71e41c761e3
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-07-29 07:20:37 +0200

    test(core): add pdf power generator

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2019 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+from utila import SUCCESS
+from utila import run
+from utila.test import LONGRUN
+
+from tests.resources import TEST_DATA
+
+pytest_plugins = 'pytester'  # pylint: disable=invalid-name
+
+if LONGRUN:  # skip with --test=fast
+    completed = run('power --all', TEST_DATA)  # pylint:disable=C0103
+    assert completed.returncode == SUCCESS, str(completed)
