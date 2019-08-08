c4eb10ebef2a3523c005715cf7c253b54192a37c
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-08-08 19:14:20 +0200

    docs(title): add requirements for title parser

--- detector/feature/title.py
+++ detector/feature/title.py
@@ -6,7 +6,50 @@
 # use or distribution is an offensive act against international law and may
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
+"""                         TITLE PAGE EXTRACTOR
 
+The title page extractor extract information like author, type of work,
+university, date etc. out of the defined title page. Are further `decision
+unit` get these information are jduge them if there are right or something is
+missing. A further unit gives advices how to improve the `title` page.
+
+workplan:
+    title extractor -> judging unit -> adivice unit -> presentation layer
+                                    | - - - - - - - -> presentation layer
+
+Shortcuts:
+
+    TPE - Title Page Extractor
+    JU  - Judging Unit
+    AU  - Advice unit
+    PL  - Presentation layer
+
+How does the `tpe` work:
+
+Extract Strings
+        Numbers
+        Date
+
+        FontSizes
+        Boundings
+
+
+
+Requirements - "Wissenschaftliches Arbeiten" - Manuel Rene Theisen:
+
+   + Universitaets, Fakultaet, Institut, Seminar
+   + Pruefungszeit, laufendes Semenster
+   + Art: These, Seminar, Bachelor, Master, Disteration
+   + Thema
+   + Namensangabe des Pruefers
+   + Name, Vorname des Verfassers mit eventuellem akademischem Titel
+   + Matrikelnummer
+   + Studienadresse
+   + Studiengang, Fachrichtung, (Semesterzahl?)
+   + Termin der Abgabe/Einreichung
+
+
+"""
 from typing import Tuple
 
 from utila import Flag
