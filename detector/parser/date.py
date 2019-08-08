289ca8725aa7d134bac370a73a8a9eb24edd1e19
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-08-08 19:13:35 +0200

    feat(date): add parser to parse the date of title page

---
+++ detector/parser/date.py
@@ -0,0 +1,184 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2019 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+"""Parses dates from the first page of a thesis.
+
+TODO:
+    Make parser more tolerant, if nothing matches, reduces accuracy due
+    reduce the search pattern like Januar -> Janua, Janu, .. Jan..
+
+"""
+
+from calendar import weekday
+from collections import namedtuple
+from functools import partial
+from re import search as re_search
+
+TitleDate = namedtuple('TitleDate', 'year month day location valid raw')
+
+
+def parse(raw: str) -> TitleDate:
+    """Convert `raw` line to `TitleDate`
+
+    Args:
+        raw(str): extracted line of pdf document
+    Returns:
+        return parsed TitleDate - return None if parsing is not possible
+    """
+
+    # Require different regex
+    # Judge function to decide when having multiple results
+
+    pattern = [
+        location_comman_day_month_year,
+        simple_alpha_date,
+        simple_month_year_date,
+        simple_date,
+    ] + [partial(simple_alpha_date, reduce=index) for index in range(7)]
+
+    parsed = [parser(raw) for parser in pattern]
+
+    parsed = [item for item in parsed if item]  # remove non matches
+    # use longest matching pattern
+    parsed = sorted(parsed, key=lambda x: len(x.raw), reverse=True)
+    if not parsed:
+        return None
+    return parsed[0]
+
+
+def validate_date(year, month, day):
+    try:
+        weekday(year, month, day)
+    except ValueError:
+        return False
+    return True
+
+
+MONTH = [
+    'Januar',
+    'Februar',
+    'März',
+    'April',
+    'Mai',
+    'Juni',
+    'Juli',
+    'August',
+    'September',
+    'Oktober',
+    'November',
+    'Dezember',
+]
+
+MONTH_GROUP = r'(?P<month>' + '|'.join(MONTH) + ')'
+
+SIMPLE_DATE = r'(?P<day>\d{1,2})\.(?P<month>\d{1,2})\.(?P<year>\d{4})'
+
+SIMPLE_ALPHA_DATE = r'(?P<day>\d{1,2})([\.|,]) ' + MONTH_GROUP\
+                                                 + r' (?P<year>\d{4})'
+
+SIMPLE_MONTH_YEAR = MONTH_GROUP + r' (\d{4})'
+
+LOCATION_COMMA_DAY_MONTH_YEAR = r'(?P<location>\w+), (den ){0,1}('\
+                                                      + SIMPLE_ALPHA_DATE + ')'
+
+
+def simple_date(raw):
+    res = re_search(SIMPLE_DATE, raw)
+    if not res:
+        return None
+    res = res.groups()
+    valid = len(res[0]) == len(res[1]) == 2
+    raw = '%s.%s.%s' % res
+    year, month, day = int(res[2]), int(res[1]), int(res[0])
+    result = TitleDate(
+        year=year,
+        month=month,
+        day=day,
+        location=None,
+        valid=valid,
+        raw=raw,
+    )
+    return result
+
+
+def reduce_word(word, count):
+    reduced = word[0:len(word) - count]  # TODO: improve this
+    if len(reduced) <= 3:
+        return word[0:3]
+    return reduced
+
+
+def simple_alpha_date(raw, reduce: int = 0):
+    """
+    Args:
+        reduce(int):
+    Returns:
+
+    """
+    month_match = {reduce_word(item, reduce): item for item in MONTH}
+    changed_pattern = str(SIMPLE_ALPHA_DATE)
+    for key, value in month_match.items():
+        changed_pattern = changed_pattern.replace(value, key)
+    res = re_search(changed_pattern, raw)
+    if not res:
+        return None
+
+    matched = res.string[res.span()[0]:res.span()[1]]
+    day = int(res['day'])
+    collected = month_match[res['month']]
+    month = MONTH.index(collected) + 1
+    year = int(res['year'])
+    valid = len(res['day']) == 2
+    result = TitleDate(
+        year=year,
+        month=month,
+        day=day,
+        location=None,
+        valid=valid,
+        raw=matched,
+    )
+    return result
+
+
+def simple_month_year_date(raw,):
+    res = re_search(SIMPLE_MONTH_YEAR, raw)
+    if not res:
+        return None
+    res = res.groups()
+    raw = '%s %s' % res
+    month = MONTH.index(res[0]) + 1
+    year = int(res[1])
+    result = TitleDate(
+        year=year,
+        month=month,
+        day=None,
+        location=None,
+        valid=True,
+        raw=raw,
+    )
+    return result
+
+
+def location_comman_day_month_year(raw):
+    res = re_search(LOCATION_COMMA_DAY_MONTH_YEAR, raw)
+    if not res:
+        return None
+    location = res['location']
+    day = int(res['day'])
+    month = MONTH.index(res['month']) + 1
+    year = int(res['year'])
+    valid = validate_date(year, month, day)
+
+    return TitleDate(
+        year=year,
+        month=month,
+        day=day,
+        location=location,
+        valid=valid,
+        raw=res.string,
+    )
---
+++ tests/detector_/test_parser_date.py
@@ -0,0 +1,65 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2019 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+from pytest import mark
+
+from detector.parser.date import TitleDate
+from detector.parser.date import parse
+
+
+@mark.parametrize('raw, expected', [
+    (
+        'Berlin, den 4. Juni 2010',
+        TitleDate(2010, 6, 4, 'Berlin', True, 'Berlin, den 4. Juni 2010'),
+    ),
+    (
+        'Berlino, 19. April 2016',
+        TitleDate(2016, 4, 19, 'Berlino', True, 'Berlino, 19. April 2016'),
+    ),
+    (
+        'Berlin, 39. April 2016',
+        TitleDate(2016, 4, 39, 'Berlin', False, 'Berlin, 39. April 2016'),
+    ),
+    (
+        '19. April 2016',
+        TitleDate(2016, 4, 19, None, True, '19. April 2016'),
+    ),
+    (
+        'Abgabedatum: 15. Jan 2010 ',
+        TitleDate(2010, 1, 15, None, True, '15. Jan 2010'),
+    ),
+    (
+        'Juli 2003 ',
+        TitleDate(2003, 7, None, None, True, 'Juli 2003'),
+    ),
+    (
+        'INFOTEH-JAHORINA Vol. 14, März 2015.',
+        TitleDate(2015, 3, 14, None, True, '14, März 2015'),
+    ),
+    (
+        ' 21.05.1999  random',
+        TitleDate(1999, 5, 21, None, True, '21.05.1999'),
+    ),
+    (
+        'date: 21.5.1999',
+        TitleDate(1999, 5, 21, None, False, '21.5.1999'),
+    ),
+    (
+        'Stand: 07.04.2013 ',
+        TitleDate(2013, 4, 7, None, True, '07.04.2013'),
+    ),
+    (
+        'First printing, Oktober 2004 ',
+        TitleDate(2004, 10, None, None, True, 'Oktober 2004'),
+    ),
+])
+def test_parse_date(raw, expected):
+    parsed = parse(raw)
+
+    assert parsed == expected, str(parsed)
