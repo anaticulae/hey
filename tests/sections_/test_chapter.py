c1eeb7ff482a20bed451171bb137111eed8a3ee3
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2019-09-22 23:04:21 +0200

    refactor(test): parametrize test to prepare for extending

--- tests/sections_/test_chapter.py
+++ tests/sections_/test_chapter.py
@@ -7,57 +7,64 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
+import pytest
 import serializeraw
 from serializeraw import load_document
 from serializeraw import load_toc
 
-from groupme.feature.numbers import load_textposition
-from hey.textnavigator.navigator import create_pagetextnavigators
-from sections.feature.chapter import space_between_header_and_first_line
-from tests.resources import RESTRUCT_TEXT
-from tests.resources import RESTRUCT_TEXT_POSITION
-from tests.resources import RESTRUCT_TOC
+import groupme.feature.numbers
+import hey.textnavigator.navigator
+import sections.feature.chapter
+import tests.resources
 
 
-def test_chapter_extract():
-    document = load_document(RESTRUCT_TEXT)
-    position = load_textposition(RESTRUCT_TEXT_POSITION)
-    tocs = load_toc(RESTRUCT_TOC)
-
-    navigators = create_pagetextnavigators(
-        text=document,
-        text_positions=position,
-    )
-
-    result = space_between_header_and_first_line(
-        navigators,
-        tocs,
-    )
-
-    expected = [6, 8, 10, 12, 18, 20, 22, 24]
+@pytest.mark.parametrize('document, position, toc, expected', [
+    pytest.param(
+        tests.resources.RESTRUCT_TEXT,
+        tests.resources.RESTRUCT_TEXT_POSITION,
+        tests.resources.RESTRUCT_TOC,
+        [6, 8, 10, 12, 18, 20, 22, 24],
+        id='restruct',
+    ),
+])
+def test_sections_chapter_extract(document, position, toc, expected):
+    result = extract_chapter(document, position, toc)
 
     pages = [item.page for item in result]
 
     assert pages == expected
 
 
-def test_chapter_dump_and_load_detection():
-    # load
-    document = load_document(RESTRUCT_TEXT)
-    position = load_textposition(RESTRUCT_TEXT_POSITION)
-    tocs = load_toc(RESTRUCT_TOC)
-    # TODO: Think about how to handle this, invocation order of features?
-    navigators = create_pagetextnavigators(
-        text=document,
-        text_positions=position,
-    )
-
-    result = space_between_header_and_first_line(
-        navigators,
-        tocs,
-    )
+@pytest.mark.parametrize('document, position, toc', [
+    pytest.param(
+        tests.resources.RESTRUCT_TEXT,
+        tests.resources.RESTRUCT_TEXT_POSITION,
+        tests.resources.RESTRUCT_TOC,
+        id='restruct',
+    ),
+])
+def test_sections_chapter_dump_and_load_detection(document, position, toc):
+    result = extract_chapter(document, position, toc)
 
     dumped = serializeraw.dump_likelihood(result)
     loaded = serializeraw.load_likelihood(dumped)
 
     assert loaded == result
+
+
+def extract_chapter(document, position, toc):
+    # load
+    document = load_document(document)
+    position = groupme.feature.numbers.load_textposition(position)
+    tocs = load_toc(toc)
+
+    navigators = hey.textnavigator.navigator.create_pagetextnavigators(
+        text=document,
+        text_positions=position,
+    )
+
+    result = sections.feature.chapter.space_between_header_and_first_line(
+        navigators,
+        tocs,
+    )
+    return result
