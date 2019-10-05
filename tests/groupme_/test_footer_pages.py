373fe48eef6ecb08878fe8e12c12c7e422b401fc
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2019-10-05 20:33:43 +0200

    test(footer): add test to extract pagenumber footer from two examples
    Prepare implementation - Not working yet.

---
+++ tests/groupme_/test_footer_pages.py
@@ -0,0 +1,57 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2019 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import pytest
+import serializeraw
+
+import groupme.footer.pages
+import tests.resources
+
+
+@pytest.mark.parametrize('document, pages, expected_pagenumbers', [
+    pytest.param(
+        tests.resources.HOWTO_ARGPARSE,
+        tests.resources.HOWTO_ARGPARSE_PAGE_COUNT,
+        tests.resources.HOWTO_ARGPARSE_PAGE_COUNT,
+        id='howtoargparse',
+        marks=pytest.mark.xfail(reason='not implemented yet')),
+    pytest.param(
+        tests.resources.TECHNICAL_24PAGES,
+        tests.resources.TECHNICAL_24PAGES_PAGE_COUNT,
+        tests.resources.TECHNICAL_24PAGES_PAGE_COUNT - 1,
+        id='technical24pages',
+        marks=pytest.mark.xfail(reason='not implemented yet')),
+])
+def test_footer_pagenumber_strategy(
+        document,
+        pages,
+        expected_pagenumbers,
+):
+    pages = tuple(range(pages))
+    horizontallines = serializeraw.load_horizontals(
+        tests.resources.horizontals(document),
+        pages,
+    )
+    sizeandborder = serializeraw.load_pageborders(
+        tests.resources.sizeandborder(document),
+        pages,
+    )
+
+    pagenumbers = serializeraw.load_pagenumbers(
+        tests.resources.pagenumbers(document),
+        pages,
+    )
+    strategy = groupme.footer.pages.PageNumberStrategy(
+        horizontallines,
+        sizeandborder,
+        pagenumbers,
+    )
+    result = strategy.result()
+    assert result, result
+    assert len(result) == expected_pagenumbers, result
