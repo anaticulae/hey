d528ded6b2955446d3c9d3d5ec9f244013cf0418
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2019-10-17 10:10:46 +0200

    test(footer): extend fixed footer strategy test
    + restructed test
    + bachelor111 test

--- tests/groupme_/test_footer_fixed.py
+++ tests/groupme_/test_footer_fixed.py
@@ -7,6 +7,7 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
+import pytest
 import serializeraw
 import utila
 
@@ -14,25 +15,45 @@ import groupme.footer.fixed
 import tests.resources
 
 
-def test_groupme_footer_fixed_restructed_extract():
+def _restructed():
     horizontals = tests.resources.horizontals(tests.resources.RESTRUCT)
     horizontals = serializeraw.load_horizontals(horizontals)
 
     sizeandborder = tests.resources.sizeandborder(tests.resources.RESTRUCT)
     sizeandborder = serializeraw.load_pageborders(sizeandborder)
+
     pageheight = utila.select_page(sizeandborder, 0).size.height
 
     top, bottom = groupme.footer.fixed.extract_common_footer(
         horizontals=horizontals,
         pageheight=pageheight,
     )
+    return horizontals, pageheight, top, bottom
+
+
+def test_groupme_footer_fixed_restructed_extract_common_footer():
+    _, __, top, bottom = _restructed()
     assert top  # document has header
     assert bottom  # document has footer
-
     assert top < bottom
 
 
-def test_groupme_footer_fixed_bachelor111page():
+def test_groupme_footer_fixed_restructed_extract_page_footerheader():
+    horizontals, pageheight, top, bottom = _restructed()
+
+    extracted = groupme.footer.fixed.extract_page_footerheader(
+        horizontals,
+        top,
+        bottom,
+        pageheight,
+    )
+    allfooter = [
+        item.footer is not None for item in extracted if item.page >= 2
+    ]
+    assert all(allfooter), utila.log_raw(allfooter)
+
+
+def _bachelor111():
     horizontals = tests.resources.horizontals(tests.resources.BACHELOR_111PAGES)
     horizontals = serializeraw.load_horizontals(horizontals)
 
@@ -44,6 +65,25 @@ def test_groupme_footer_fixed_bachelor111page():
         horizontals=horizontals,
         pageheight=pageheight,
     )
+    return horizontals, pageheight, top, bottom
+
+
+def test_groupme_footer_fixed_bachelor111page_extract_common_footer():
+    _, __, top, bottom = _bachelor111()
     assert top  # document has header
     assert bottom  # document has footer
     assert top < bottom
+
+
+@pytest.mark.xfail(reason='header collector is not good enough')
+def test_groupme_footer_fixed_bachelor111page_extract_page_footerheader():
+    horizontals, pageheight, top, bottom = _bachelor111()
+
+    extracted = groupme.footer.fixed.extract_page_footerheader(
+        horizontals,
+        top,
+        bottom,
+        pageheight,
+    )
+    header = [item.header for item in extracted if item.header]
+    assert len(header) == 94, utila.log_raw(header)
