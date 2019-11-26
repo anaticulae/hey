e9eee8b00cfd836e3e8d2412fa70c78860c84fcc
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2019-11-26 14:55:56 +0100

    test(groupme): add test to verify quality of bachelor111 footer extraction

---
+++ tests/groupme_/footerheader/test_quality.py
@@ -0,0 +1,33 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2019 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import serializeraw
+import utila
+
+import groupme.feature.footer
+import tests.resources
+
+
+def test_groupme_footerheader_quality_bachelor111():
+    dumped = groupme.feature.footer.work(
+        tests.resources.text(tests.resources.BACHELOR_111PAGES),
+        tests.resources.text_positions(tests.resources.BACHELOR_111PAGES),
+        tests.resources.horizontals(tests.resources.BACHELOR_111PAGES),
+        tests.resources.sizeandborder(tests.resources.BACHELOR_111PAGES),
+        tests.resources.pagenumbers(tests.resources.BACHELOR_111PAGES),
+    )
+    result = serializeraw.load_headerfooter(dumped)
+
+    pages = list(range(1, 5))
+    selected = [utila.select_page(result, page) for page in pages]
+
+    extracted_pages = [item.page for item in selected]
+
+    assert len(selected) == len(pages)
+    assert extracted_pages == pages
