18e935b393076d110470e3cee9b2534378539259
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-02-22 17:03:45 +0100

    feat(groupme): add distance feature
    + add unit test
    
    Solves release plan goal

---
+++ tests/groupme_/distance/test_distance.py
@@ -0,0 +1,69 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2020 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import groupme.feature.distance
+import groupme.path
+import rawmaker.path
+import tests.resources
+
+
+def pyporting(pages: tuple = None):
+    source = tests.resources.HOWTO_PYPORTING
+    area = groupme.path.area(source)
+    text = rawmaker.path.text(source)
+    textposition = rawmaker.path.textposition(source)
+    loaded = groupme.feature.distance.load(
+        area,
+        text,
+        textposition,
+        pages=pages,
+    )
+    return loaded
+
+
+def test_distance_pyport_page0():
+    loaded = pyporting(pages=(0))
+    distances = groupme.feature.distance.determine_distances(loaded)
+    first = distances[0].content[0]
+    assert first.after >= 40, first.after
+    assert first.before <= -16, first.before
+
+
+def test_distance_pyport_page3():
+    loaded = pyporting(pages=(3))
+    distances = groupme.feature.distance.determine_distances(loaded)
+    page = distances[0].content
+    assert len(page) == 1, page
+    first = page[0]
+    assert first.before < 0, first
+    assert first.after > 0, first
+
+
+def test_distance_pyport_page5():
+    loaded = pyporting(pages=(5))
+    distances = groupme.feature.distance.determine_distances(loaded)
+    page = distances[0].content
+    assert len(page) == 4, page
+    assert all((item.after is None or item.after >= 0.0 for item in page))
+    assert all((item.before is None or item.before < 0.0 for item in page))
+
+
+def test_distance_pyport():
+    loaded = pyporting()
+    distances = groupme.feature.distance.determine_distances(loaded)
+    assert len(distances) == 3, distances
+
+
+def test_distance_dump_load():
+    data = pyporting()
+    distances = groupme.feature.distance.determine_distances(data)
+
+    dumped = groupme.feature.distance.dump_distance(distances)
+    loaded = groupme.feature.distance.load_distance(dumped)
+    assert loaded == distances
