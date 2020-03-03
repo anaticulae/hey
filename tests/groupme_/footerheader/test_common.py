aa343340c84da287dcb3dd17ea615764a522ff66
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-03-03 15:01:41 +0100

    test(footer): ensure to extract selective footer correctly

---
+++ tests/groupme_/footerheader/test_common.py
@@ -0,0 +1,35 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2020 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+import iamraw.path
+import serializeraw
+
+import tests.groupme_
+import tests.resources
+
+
+def test_footer_regression_common_strategy(testdir, monkeypatch):
+    """There was a bug in handling selective --pages=1 correctly. In the
+    old implementation the page height of page zero was used for the
+    whole document. Therefore selecting page one produces an None access
+    error."""
+    root = testdir.tmpdir
+    source = tests.resources.BACHELOR37
+    page = 1
+    cmd = f'-i {source} -o {root} --footer --pages={page}'
+    tests.groupme_.run_success(cmd, monkeypatch=monkeypatch)
+
+    path = iamraw.path.headerfooters(root)
+    headerfooter = serializeraw.load_headerfooter(path)
+    assert len(headerfooter) == 1
+
+    # Hint: this result is not produced by common strategy
+    headerfooter = headerfooter[0]
+    assert headerfooter.header is None
+    assert headerfooter.footer
+    assert headerfooter.page == page
