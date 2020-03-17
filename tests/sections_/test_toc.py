ddec733600adcf564e2ae58d7d0bce8b9a9bef0f
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-03-17 21:05:23 +0100

    fest(toc): add test to verify multi toc pages

--- tests/sections_/test_toc.py
+++ tests/sections_/test_toc.py
@@ -7,15 +7,27 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
-from pytest import approx
+import iamraw.path
+import pytest
+import serializeraw
 
-from sections.feature.toc import extract_toc_likelihood
+import sections.feature.toc
+import tests.resources
 # pylint:disable=W0611
 from tests.fixtures.restruct import restructured_text
 
 
 #pylint:disable=W0621
 def test_extract_toc_likelihood(restructured_text):
-    extracted = extract_toc_likelihood(restructured_text)
+    extracted = sections.feature.toc.extract_toc_likelihood(restructured_text)
     extracted = [item.content.value for item in extracted]
-    assert sum(extracted) == approx(1.0)
+    assert sum(extracted) == pytest.approx(1.0)
+
+
+def test_extract_toc_likelihood_bachelor63():
+    text = iamraw.path.text(tests.resources.BACHELOR63, prefix='oneline')
+    text = serializeraw.load_document(text, pages=(0, 1, 2, 3, 4, 5, 6, 7))
+
+    extracted = sections.feature.toc.extract_toc_likelihood(text)
+    extracted = [item.content.value for item in extracted]
+    assert sum(extracted) == pytest.approx(1.0)
