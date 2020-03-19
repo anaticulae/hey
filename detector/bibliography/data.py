4679a40a3b9c3539cc8a21de656dc230466456e1
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-03-19 22:28:04 +0100

    feat(bib): make bibliography sortable
    Solves release plan goal

--- detector/bibliography/data.py
+++ detector/bibliography/data.py
@@ -6,10 +6,24 @@
 # use or distribution is an offensive act against international law and may
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
+"""BibliographyReference
+=====================
 
+Sorting
+-------
+
+We sort by family name and as tybreaker by year. If no name is given, we
+use `o. V. = ohne Verfasser` instead. If no year is given, we sort it
+after the items with year.
+
+# TODO: ADD OTHER SORTING AS THEISSEN recommends
+"""
+
+import contextlib
 import dataclasses
 import typing
 
+import texmex.alpha
 import utila
 import yaml
 
@@ -23,6 +37,34 @@ class BibliographyReference:
     title: str = None
     year: int = None
     authors: typing.List[str] = dataclasses.field(default_factory=list)
+
+    def __lt__(self, value):  # pylint:disable=too-many-return-statements
+        """See rules in module doc `sorting`."""
+        author = self.author if self.author else 'o. V.'
+        value_author = value.author if value.author else 'o. V.'
+        author = texmex.alpha.replace(author).lower()
+        value_author = texmex.alpha.replace(value_author).lower()
+        if author == value_author:
+            if self.year is None:
+                return False
+            if value.year is None:
+                return True
+            return self.year < value.year
+        return author < value_author
+
+    @classmethod
+    def create(cls, author: str, title: str = '', year: int = 2000):
+        author = tuple(author.split(' ', maxsplit=1))
+        with contextlib.suppress(TypeError):
+            year = int(year)
+        return cls(authors=[author], title=title, year=year)
+
+    @property
+    def author(self) -> str:
+        """Return family of first author."""
+        with contextlib.suppress(IndexError):
+            return self.authors[0][0]  # pylint:disable=E1136
+        return None
 
 
 BibliographyReferences = typing.List[BibliographyReference]
---
+++ tests/detector_/bibliography/test_data.py
@@ -0,0 +1,45 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2020 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import detector.bibliography.data as dbd
+
+
+def test_sort_byname():
+    example = [
+        dbd.BibliographyReference.create('Mueller Erwin'),
+        dbd.BibliographyReference.create('Arnold Anton'),
+        dbd.BibliographyReference.create('Fahrendholz Konrad'),
+    ]
+    result = sorted(example)
+    expected = [example[1], example[2], example[0]]
+    assert result == expected
+
+
+def test_sort_byyear():
+    year = [
+        dbd.BibliographyReference.create('Fahrendholz Konrad', year=2016),
+        dbd.BibliographyReference.create('Fahrendholz Konrad', year=None),
+        dbd.BibliographyReference.create('Fahrendholz Konrad', year=1987),
+    ]
+    result = sorted(year)
+    expected = [year[2], year[0], year[1]]
+    assert result == expected
+
+
+def test_sort_bynoname():
+    # pylint:disable=C0103
+    ov = [
+        dbd.BibliographyReference(year=None),
+        dbd.BibliographyReference(year=2016),
+        dbd.BibliographyReference.create('Fahrendholz Konrad', year=None),
+        dbd.BibliographyReference.create('Fahrendholz Konrad', year=1987),
+    ]
+    result = sorted(ov)
+    expected = [ov[3], ov[2], ov[1], ov[0]]
+    assert result == expected
