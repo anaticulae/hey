04393de733aa04501cfa480c99ba9827dd4a9c7a
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-05-15 18:11:55 +0200

    refactor(test): simplify test code

--- tests/textstyle_/test_cluster.py
+++ tests/textstyle_/test_cluster.py
@@ -16,58 +16,64 @@ import textstyle.parser
 import textstyle.utils
 
 
-def master116(pages: tuple):
-    navigators = serializeraw.create_pagetextnavigators_frompath(
-        tests.resources.MASTER72,
+def navigators(source: str, pages: tuple):
+    loaded = serializeraw.create_pagetextnavigators_frompath(
+        source,
         prefix='oneline',
         pages=pages,
     )
-    parsed = textstyle.parser.parses(navigators)
+    parsed = textstyle.parser.parses(loaded)
     flat = textstyle.utils.flatten(parsed)
     return flat
 
 
 @pytest.fixture
-def master116_text_flat():
-    return master116(pages=tuple(range(3, 86)))
+def master72_text_flat():
+    return navigators(
+        source=tests.resources.MASTER72,
+        pages=tuple(range(3, 86)),
+    )
 
 
 @pytest.fixture
-def master116_text_flat_small():
-    return master116(pages=tuple(range(3, 15)))
+def master72_text_flat_small():
+    return navigators(
+        source=tests.resources.MASTER72,
+        pages=tuple(range(3, 15)),
+    )
 
 
-def test_cluster_size(master116_text_flat):  # pylint:disable=W0621
-    data = master116_text_flat
+def test_cluster_size(master72_text_flat):  # pylint:disable=W0621
+    data = master72_text_flat
     selection = (textstyle.cluster.page.ClusterProperty.SIZE,)
     clustered = textstyle.cluster.page.cluster(data, selection=selection)
-    assert len(master116_text_flat) >= 2000
+    assert len(master72_text_flat) >= 2000
     # six different font size cluster
     assert len(clustered) == 7
 
 
-def test_cluster_extract_default_textsize(master116_text_flat):  # pylint:disable=W0621
-    data = master116_text_flat
+def test_cluster_extract_default_textsize(master72_text_flat):  # pylint:disable=W0621
+    data = master72_text_flat
     default_text = textstyle.cluster.page.text(data)
 
     # document text size
     assert default_text[0] == 12.0
 
 
-def test_cluster_extract_pagenumber(master116_text_flat_small):  # pylint:disable=W0621
-    pagenumber = textstyle.cluster.page.pagenumber(master116_text_flat_small)
+def test_cluster_extract_pagenumber(master72_text_flat_small):  # pylint:disable=W0621
+    pagenumber = textstyle.cluster.page.pagenumber(master72_text_flat_small)
     assert pagenumber[0] == 11.04, pagenumber
 
 
-def test_cluster_extract_headlines_small(master116_text_flat_small):  # pylint:disable=W0621
-    headlines = textstyle.cluster.page.headlines(master116_text_flat_small)
+def test_cluster_extract_headlines_small(master72_text_flat_small):  # pylint:disable=W0621
+    headlines = textstyle.cluster.page.headlines(master72_text_flat_small)
     assert len(headlines) == 2
     assert headlines[0][0] == 15.96
     assert headlines[1][0] == 14.04
 
 
-def test_cluster_extract_headlines_all(master116_text_flat):  # pylint:disable=W0621
-    headlines = textstyle.cluster.page.headlines(master116_text_flat)
+def test_cluster_extract_headlines_all(master72_text_flat):  # pylint:disable=W0621
+    headlines = textstyle.cluster.page.headlines(master72_text_flat)
     assert len(headlines) == 3
     assert headlines[0][0] == 15.96, str(headlines)
     assert headlines[1][0] == 14.04, str(headlines)
