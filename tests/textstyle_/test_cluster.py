babddcac6670e832a616e89fff9ab48bacd9ca4c
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-05-15 16:17:36 +0200

    feat(textstyle): add first and second headline extractor

--- tests/textstyle_/test_cluster.py
+++ tests/textstyle_/test_cluster.py
@@ -43,7 +43,7 @@ def test_cluster_size(master116_text_flat):  # pylint:disable=W0621
     clustered = textstyle.cluster.page.cluster(data, selection=selection)
     assert len(master116_text_flat) >= 2000
     # six different font size cluster
-    assert len(clustered) == 6
+    assert len(clustered) == 7
 
 
 def test_cluster_extract_default_textsize(master116_text_flat):  # pylint:disable=W0621
@@ -57,3 +57,18 @@ def test_cluster_extract_default_textsize(master116_text_flat):  # pylint:disabl
 def test_cluster_extract_pagenumber(master116_text_flat_small):  # pylint:disable=W0621
     pagenumber = textstyle.cluster.page.pagenumber(master116_text_flat_small)
     assert pagenumber[0] == 11.04, pagenumber
+
+
+def test_cluster_extract_headlines_small(master116_text_flat_small):  # pylint:disable=W0621
+    headlines = textstyle.cluster.page.headlines(master116_text_flat_small)
+    assert len(headlines) == 2
+    assert headlines[0][0] == 15.96
+    assert headlines[1][0] == 14.04
+
+
+def test_cluster_extract_headlines_all(master116_text_flat):  # pylint:disable=W0621
+    headlines = textstyle.cluster.page.headlines(master116_text_flat)
+    assert len(headlines) == 3
+    assert headlines[0][0] == 15.96, str(headlines)
+    assert headlines[1][0] == 14.04, str(headlines)
+    assert headlines[2][0] == 12.0, str(headlines)
--- textstyle/cluster/page.py
+++ textstyle/cluster/page.py
@@ -10,7 +10,10 @@
 import enum
 import typing
 
+import utila
+
 import hey.classificator
+import textstyle
 
 
 class ClusterProperty(enum.Enum):
@@ -25,9 +28,11 @@ ClusterPropertySelection = typing.List[ClusterProperty]
 
 
 def cluster(
-        items,
+        items: textstyle.TextProperties,
         selection: ClusterPropertySelection = None,
         validator: callable = None,
+        *,
+        minsize: int = 5,  # TODO: HOLY VALUE
 ):
     if selection:
         selection = set(selection)
@@ -49,28 +54,96 @@ def cluster(
     clustered = hey.classificator.determine_cluster(
         items,
         classifier=classifier,
+        min_elements=minsize,
     )
     return clustered
 
 
-def text(flats):
+def text(flats, returncluster: bool = False):
     clustered = cluster(flats, (ClusterProperty.SIZE, ClusterProperty.FONT))
-    return bestmatch(clustered)
+    result = bestmatch(clustered)
+    if returncluster:
+        return result, clustered[0]
+    return result
 
 
-def pagenumber(flats):
+def pagenumber(flats, returncluster: bool = False):
     clustered = cluster(
         flats,
         (ClusterProperty.SIZE, ClusterProperty.FONT),
         validator=lambda item: item.length <= 6,
     )
-    assert len(clustered) == 1, len(clustered)
-    return bestmatch(clustered)
+    # assert len(clustered) == 1, len(clustered)
+    result = bestmatch(clustered)
+    if returncluster:
+        return result, clustered[0]
+    return result
 
 
-def bestmatch(clustered):
-    largest_cluster_first_item = clustered[0].content[0]
+def bestmatch(clustered, number: int = 0):
+    largest_cluster_first_item = clustered[number].content[0]
     size = largest_cluster_first_item.size
     font = largest_cluster_first_item.font
+    before = utila.mode([item.before for item in clustered[number].content])
+    after = utila.mode([item.after for item in clustered[number].content])
     length = len(clustered[0])
-    return (size, font, length)
+    return (size, font, length, (before, after))
+
+
+def headlines(
+        flats: textstyle.TextProperties,
+        min_headline_count: int = 3,
+        greater_than_text: bool = True,
+):
+    _text = text(flats, returncluster=True)
+    _pagenumber = pagenumber(flats, returncluster=True)
+
+    flats = remove(flats, _text[1])
+    flats = remove(flats, _pagenumber[1])
+
+    textsize = _text[0][0]
+    text_before, text_after = _text[0][3]
+
+    if greater_than_text:
+        flats = [item for item in flats if item.size >= textsize]
+
+    def valid_headline(item) -> bool:
+        if item.before is None:
+            return True
+        if item.before <= text_before * 1.2:  # TODO: HOLY VALUE
+            return False
+        if item.after is None:
+            return False
+        if item.after <= text_after * 1.2:  # TODO: HOLY VALUE
+            return False
+        return True
+
+    clustered = cluster(
+        flats,
+        (ClusterProperty.SIZE,),
+        validator=valid_headline,
+        minsize=min_headline_count,
+    )
+
+    largest_font_size = sorted(
+        clustered,
+        key=lambda x: x.content[0].size,
+        reverse=True,
+    )
+
+    result = []
+    for index in range(5):
+        # analyse maximal five headline levels
+        try:
+            matched = bestmatch(largest_font_size, number=index)  # pylint:disable=C0103
+        except IndexError:
+            break
+        else:
+            result.append(matched)
+    return result
+
+
+def remove(flats, toremove: list):
+    toremove = set(toremove)
+    flats = [item for item in flats if item not in toremove]
+    return flats
