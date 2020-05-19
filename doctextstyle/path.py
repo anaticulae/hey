28b69ed89de56d2073907b85dfe27107d8016ec0
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-05-19 08:36:26 +0200

    feat(path): add module to determines paths of written content

---
+++ doctextstyle/path.py
@@ -0,0 +1,14 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2020 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import utila
+
+
+def textstyle(path: str, prefix: str = '') -> str:
+    return utila.pathconnector(path, 'doctextstyle', 'textstyle', prefix)
