8291b18cbf45e4686b6668668df793cfdc14b93c
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-06-06 19:48:36 +0200

    feat(authors): add multiple authors pattern parser

--- detector/bibliography/reference/authors.py
+++ detector/bibliography/reference/authors.py
@@ -8,6 +8,25 @@
 # =============================================================================
 
 
+def parses(raw: str):
+    """\
+    >>> parses('Becker, W.; Ulrich, P.; Botzkowski, T.; Eurich, S.')
+    [['Becker', 'W.'], ['Ulrich', 'P.'], ['Botzkowski', 'T.'], ['Eurich', 'S.']]
+    >>> parses('AASLID, R. - BRUBAKK, AO.')
+    [['AASLID', 'R.'], ['BRUBAKK', 'AO.']]
+    >>> parses('Beirness, D. and Vogel-Sprott, M.')
+    [['Beirness', 'D.'], ['Vogel-Sprott', 'M.']]
+    """
+    free = freeand(raw)
+    semicolon = simple(raw)
+    hyphen = simple(raw, extern='-', intern=',')
+
+    result = [free, semicolon, hyphen]
+    balanced = [balance(item) for item in result]
+    max_balance = maxindex(balanced)
+    return result[max_balance]
+
+
 def simple(raw: str, extern: str = ';', intern: str = ','):
     """\
     >>> simple('Becker, W.; Ulrich, P.')
@@ -42,3 +61,33 @@ def freeand(raw: str):
         else:
             result.append([item])
     return result
+
+
+def balance(authors):
+    if not authors:
+        return None
+    common = [valid_author(author) for author in authors]
+    valid = len([item for item in common if item])
+    ratio = valid / len(common)
+    return ratio
+
+
+def valid_author(author):
+    if len(author) != 2:
+        return False
+    for item in author:
+        if ';' in item:
+            return False
+        if ',' in item:
+            return False
+    return True
+
+
+def maxindex(items):
+    if not items:
+        return None
+    current = 0
+    for index, value in enumerate(items[1:], start=1):
+        if value > items[current]:
+            current = index
+    return current
