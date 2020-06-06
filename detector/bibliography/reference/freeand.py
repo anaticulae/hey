68baf18aa1b41bbdcec0630b06048a16820a8485
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-06-06 18:25:10 +0200

    feat(bib): add freeand pattern parser

---
+++ detector/bibliography/reference/freeand.py
@@ -0,0 +1,96 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2020 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+"""BibRef AND(&) Parser
+====================
+
+.. code-block:: none
+
+    Schmidt, R., Dettmeyer, R., Padosch, S. & Madea, B. (2000b). A
+    review of the literature on the Effects of Low Doses of Alcohol on
+    driving-Related Skills. National Highway Traffic Safety
+    Administration (NHTS), Southern California Research Institute,
+    Washington.
+
+Authors
+-------
+
+Correct
+~~~~~~~
+
+* Schmidt, R., Dettmeyer, R., Padosch, S. & Madea, B.
+
+Failures
+~~~~~~~~
+
+* Schnabel, Eva
+* Sdao-Jarvie, K. and Vogel-Sprott, M.
+
+"""
+
+import re
+
+import iamraw
+
+AND = r"""
+    (?P<authors>.+)
+    \(
+        (?P<year>\d{4})
+        (?P<number>a|b|c|d){0,1}  # optional char
+    \)
+    [ ]{0,5}                      # remove trailing whitespaces
+    \.{0,1}                       # remove dot
+    [ ]{0,5}                      # remove trailing whitespaces
+    (?P<content>.+)
+"""
+
+
+def parse_longtext(content: str) -> iamraw.BibliographyReference:
+    content = content.replace('\n', ' ')
+    matched = re.search(AND, content, re.VERBOSE)
+    if not matched:
+        return None
+
+    authors = parse_authors(matched['authors'])
+    year = int(matched['year'])
+    number = matched['number'] if matched['number'] else None
+
+    try:
+        title, rest = matched['content'].split('.', maxsplit=1)  # pylint:disable=W0612
+    except ValueError:
+        title, rest = None, matched['content']
+
+    result = iamraw.BibliographyReference(
+        authors=authors,
+        number=number,
+        title=title,
+        year=year,
+        raw=content,
+    )
+    return result
+
+
+def parse_authors(raw: str):
+    extracted = []
+    try:
+        # TODO: SOLVE `AND` PROBLEM ON OTHER PLACE?
+        left, right = raw.split(' and ') if ' and ' in raw else raw.split('&')
+        extracted.extend(left.split(','))
+        extracted.extend(right.split(','))
+    except ValueError:
+        extracted.extend(raw.split(','))
+    if not extracted:
+        return None
+    result = [[extracted[0]]]
+    for item in extracted[1:]:
+        item = item.strip()
+        if len(result[-1]) == 1:
+            result[-1].append(item)
+        else:
+            result.append([item])
+    return result
---
+++ tests/detector_/bibliography/test_reference_and_parser.py
@@ -0,0 +1,73 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2020 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import pytest
+
+import detector.bibliography.reference.freeand as freeand
+
+LONGTEXT = """\
+Rumph, H.-J., Hapke, U. & John, U. (1997). Lübecker Alkoholabhängigkeits- und
+-missbrauchs-Screening-Test. Göttingen: Hogrefe Verlag GmbH & Co. KG.
+
+Schmidt, R., Dettmeyer, R., Padosch, S. & Madea, B. (2004). Beweiswert
+rechtsmedizinischer Begutachtungskriterien zur Feststellung der relativen
+alkoholbedingten Fahruntüchtigkeit. Blutalkohol, 41, 1-10.
+
+Schnabel, Eva (2011). Alcohol and driving-related performance - A
+comprehensive meta-analysis focussing the significance of the
+nonsignificant. Unveröffentlichte Dissertation, Philosophische Fakultät
+II der Julius Maximilians-Universität, Würzburg.
+""".split('\n\n')
+
+
+@pytest.mark.parametrize('text, title, authors, pages, year, publisher', [
+    pytest.param(
+        LONGTEXT[0],
+        'Lübecker Alkoholabhängigkeits- und -missbrauchs-Screening-Test',
+        [['Rumph', 'H.-J.'], ['Hapke', 'U.'], ['John', 'U.']],
+        None,
+        1997,
+        None,
+        id='luebecker',
+    ),
+    pytest.param(
+        LONGTEXT[1],
+        ('Beweiswert rechtsmedizinischer Begutachtungskriterien zur '
+         'Feststellung der relativen alkoholbedingten Fahruntüchtigkeit'),
+        [['Schmidt', 'R.'], ['Dettmeyer', 'R.'], ['Padosch', 'S.'],
+         ['Madea', 'B.']],
+        None,
+        2004,
+        None,
+        id='schmidt',
+    ),
+    pytest.param(
+        LONGTEXT[2],
+        ('Alcohol and driving-related performance - A comprehensive '
+         'meta-analysis focussing the significance of the nonsignificant'),
+        [['Schnabel', 'Eva']],
+        None,
+        2011,
+        None,
+        id='schnabel',
+    ),
+])
+def test_parse_freeand_long(text, title, authors, pages, year, publisher):  # pylint:disable=W0613
+    extracted = freeand.parse_longtext(text)
+    print(extracted)
+    if title:
+        assert extracted.title == title
+    if pages:
+        assert extracted.page == pages[0]
+        if len(pages) == 2:
+            assert extracted.pageend == pages[1]
+    if year:
+        assert extracted.year == year
+    if authors:
+        assert extracted.authors == authors
