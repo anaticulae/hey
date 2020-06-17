893349bbb5d15834615d347b844514a0630abd4f
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-06-17 21:59:01 +0200

    feat(magic): add magic content path resolving method

--- magic/path.py
+++ magic/path.py
@@ -7,23 +7,8 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
-import magic.data
-import magic.path
-import tests.magic_
-import tests.resources
+import utila
 
 
-def test_magic(monkeypatch):
-    tests.magic_.run('--help', monkeypatch=monkeypatch)
-
-
-def test_master72_list(testdir, monkeypatch):
-    tests.magic_.run(
-        (f'-i {tests.resources.MASTER72_SECTIONS_AND_WORDS} '
-         f'-i {tests.resources.MASTER72} --pages=0:10'),
-        monkeypatch=monkeypatch,
-    )
-
-    path = magic.path.content(testdir.tmpdir)
-    loaded = magic.data.load_types(path)
-    assert loaded
+def content(path: str, prefix: str = '') -> str:
+    return utila.pathconnector(path, 'magic', 'content_content', prefix)
