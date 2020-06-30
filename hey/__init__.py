03f73615fc9d011ba5b71e845c7d07fe85ff1ecb
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-06-30 22:59:39 +0200

    3.5.1 automatically generated release

--- hey/__init__.py
+++ hey/__init__.py
@@ -8,7 +8,7 @@
 #==============================================================================
 import os
 
-__version__ = '3.5.0'
+__version__ = '3.5.1'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
