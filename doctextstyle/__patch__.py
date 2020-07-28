2d25562dd7403322c3af06efdc76fb27d267ed4b
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-07-28 20:27:30 +0200

    refactor(remove): remove outdated patch

--- doctextstyle/__patch__.py
+++ doctextstyle/__patch__.py
@@ -6,34 +6,3 @@
 # use or distribution is an offensive act against international law and may
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
-
-import utila
-
-
-def pnear(
-        reference,
-        current,
-        rel_tol: float = 0.0,
-        abs_tol: float = 0.05,
-) -> bool:
-    """\
-    >>> pnear(10, 8, 0.2)
-    True
-    >>> pnear(10, 8, 0.19)
-    False
-    >>> pnear(0, 0.1, rel_tol=0.02, abs_tol=0.1)
-    True
-    """
-    # TODO: UNIT WITH NEAR?
-    lower = reference * (1 - rel_tol)
-    upper = reference * (1 + rel_tol)
-    if lower <= current <= upper:
-        return True
-    lower = reference - abs_tol
-    upper = reference + abs_tol
-    if lower <= current <= upper:
-        return True
-    return False
-
-
-utila.pnear = pnear
