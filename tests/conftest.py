ddd5801acce612212e0596062c7de4b07bff1544
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-08-30 12:26:54 +0200

    chore(test): reduce required test generators

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,15 +7,10 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
-import os
-
 import power
-import utila
-import utilatest
 
 import hey
 import hey.example
-import tests.resources
 
 pytest_plugins = ['pytester', 'xdist']  # pylint: disable=invalid-name
 
@@ -26,28 +21,13 @@ RESOURCES = [
     power.MASTER099_PDF,
     power.MASTER098_PDF,
     power.BACHELOR090_PDF,
-    power.BACHELOR241_PDF,
-    power.HOMEWORK018_PDF,
-    (power.DISS264_PDF, '0:50'),
-    (power.BACHELOR056_PDF, '0:55'),
-    (power.MASTER089_PDF, '0:89'),
-    power.BACHELOR076_PDF,
     power.MASTER072_PDF,
     power.BACHELOR111_PDF,
-    power.BACHELOR037_PDF,
-    (power.BACHELOR063_PDF, '0:9,59:62'),
+    (power.BACHELOR056_PDF, '0:55'),
     power.HOMEWORK025_PDF,
-    power.DOCU27_PDF,
-    power.TECHNICAL_024,
-    power.DOCU14_PDF,
+    (power.BACHELOR063_PDF, '59:62'),
+    (power.MASTER089_PDF, '78:85'),
     power.DOCU07_PDF,
-    power.BOOK007_PDF,
-    power.DOCU09_PDF,
-    power.DOCU35_PDF,
-    (power.HOMEWORK050_PDF, '0:10'),
-    (power.ORDER009_PDF, '0:10'),
-    (power.MASTER078_PDF, '0:5'),
-    (power.MASTER083_PDF, '0:10'),
 ]
 
 WORKER = 4
@@ -66,36 +46,6 @@ def extract(resources):
         groupme=True,
         worker=WORKER,
         pages=':',
-    )
-
-
-RESOURCES_NOTITLE = tests.resources.NO_TITLE_EXAMPLE
-
-
-def extract_notitle(resources):
-    destination = power.generated(folder='notitle')
-    files = [item[0] if isinstance(item, tuple) else item for item in resources]
-    # prepare
-    without_titlepage = [
-        os.path.join(destination, f'{item}.pdf')
-        for item in utilatest.simplify_testfile_names(
-            files + [power.REPOSITORY],  # ensure correct parent
-            sort=False,
-        )
-    ]
-    # jam
-    todo = []
-    for inpath, outpath in zip(files, without_titlepage):
-        todo.append(f'jam -i {inpath} -o {outpath} --remove=0')
-    utila.run_parallel(todo)
-
-    # generate
-    hey.example.extract(
-        without_titlepage + [destination],  # ensure correct parent
-        destination,
-        groupme=True,
-        pages='0:10',
-        worker=1,
     )
 
 
--- tests/resources/__init__.py
+++ tests/resources/__init__.py
@@ -13,28 +13,7 @@ import hey
 
 power.setup(hey.ROOT)
 
-RESTRUCT_TOC_LINES = 13
-# the simple example has two 2 chapters, but there are on the same page,
-# therfore 1 page_count.
-HOWTO_PYPORTING_TOC_LINES = 12
-# porting module
-PYPORTING_CHAPTER_COUNT = 6
-
 MASTER72_SECTIONS_AND_WORDS = power.link(
     power.MASTER072_PDF,
     folder='sectionsandwords',
 )
-
-BACHELOR111_PAGE_COUNT = 111
-TECHNICAL24_PAGE_COUNT = 24
-HOWTO_ARGPARSE_PAGE_COUNT = 14
-
-NO_TITLE_EXAMPLE = [
-    power.BACHELOR111_PDF,
-    power.DOCU07_PDF,
-    power.MASTER072_PDF,
-    power.MASTER078_PDF,
-    power.DOCU09_PDF,
-    power.DOCU27_PDF,
-    power.DOCU35_PDF,
-]
