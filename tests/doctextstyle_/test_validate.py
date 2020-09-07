a9c48bd0ae611382733b899887101051a79488c2
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-09-07 18:43:05 +0200

    test(docstyle): add validation bachelor63 example

---
+++ tests/doctextstyle_/test_validate.py
@@ -0,0 +1,25 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2020 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import power
+import pytest
+
+import doctextstyle.extractor
+
+
+@pytest.mark.parametrize('source, h1, h2, h3', [
+    pytest.param(power.BACHELOR063_PDF, 15.96, 14.04, 12.0, id='bachelor63'),
+])  # pylint:disable=C0103
+def test_doctextstyle_extract_headlines(source, h1, h2, h3):
+    source = power.link(source)
+    result = doctextstyle.extractor.extract(source)
+    assert result
+    assert result.h1_size == h1
+    assert result.h2_size == h2
+    assert result.h3_size == h3
