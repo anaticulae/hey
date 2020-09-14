027a218acbb8c56a7ff36d68b567723c1923d247
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-09-14 09:38:00 +0200

    style(test): rename test to uniquely access them by name

--- tests/doctextstyle_/test_features.py
+++ tests/doctextstyle_/test_features.py
@@ -103,7 +103,7 @@ def test_cluster_extract_paragraph_small(master72_text_flat_small):
     pytest.param(power.link(power.BACHELOR111_PDF), (24, 24), id='bachelor111'),
 ])
 @utilatest.skip_longrun
-def test_cluster_extract_paragraph(source, expected):
+def test_cluster_extract_paragraph_before_and_after(source, expected):
     # TODO: VALIDATE EXPECTED LINE DISTANCE, CURRENTLY THERE ARE NOT
     # CHECKED YET.
     flat = tests.doctextstyle_.conftest.navigators(source, pages=None)
