d67bec3598baf38f7ece6a0cb4ae7090910e528e
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-10-08 19:10:15 +0200

    refactor(remove): remove outdated patch

--- doctextstyle/__patch__.py
+++ doctextstyle/__patch__.py
@@ -6,39 +6,3 @@
 # use or distribution is an offensive act against international law and may
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
-
-import dataclasses
-
-import iamraw
-
-
-@dataclasses.dataclass(unsafe_hash=True)
-class TextProperty:
-    length: int = None
-    hashed: str = None
-    size: float = None
-    font: int = None
-    before: float = None
-    after: float = None
-    top: float = None
-    bottom: float = None
-    left: float = None
-    right: float = None
-    page: int = None
-
-
-@dataclasses.dataclass(unsafe_hash=True)
-class PageTextProperties:
-    length: int = None
-    hashed: int = None
-    sizes: float = None
-    fonts: int = None
-    distances: float = None
-    ypos: float = None
-    left: float = None
-    right: float = None
-    page: int = None
-
-
-iamraw.TextProperty = TextProperty
-iamraw.PageTextProperties = PageTextProperties
