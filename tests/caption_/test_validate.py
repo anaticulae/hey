1c40d75ea072c2f9c01f38b691bbce3402abbcc2
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-01-29 16:33:23 +0100

    test(remove): remove duplicated test

--- tests/caption_/test_validate.py
+++ tests/caption_/test_validate.py
@@ -20,39 +20,8 @@ import utilatest
 import caption
 import tests.caption_.utils
 
-TABLE = os.path.join(caption.ROOT, 'tests/caption_/tables')
-file_read_table = lambda x: utila.file_read(os.path.join(TABLE, x)).strip()  # pylint:disable=C0103
-IMAGE = os.path.join(caption.ROOT, 'tests/caption_/image')
-file_read_image = lambda x: utila.file_read(os.path.join(IMAGE, x)).strip()  # pylint:disable=C0103
-
 ARCHIVE = os.path.join(caption.ROOT, 'tests/caption_/expected')
 utila.exists_assert(ARCHIVE)
-
-
-@pytest.mark.parametrize('source', [
-    pytest.param(power.BACHELOR056_PDF, id='bachelor56'),
-])
-def test_validate_caption_table(source, testdir, monkeypatch):
-    """In the current state, the table is not parsed correctly.
-
-    Table one is parsed too small. After improving the table parser,
-    labels will be parsed better.
-    TODO: UPDATE OUTDATED DOCS?
-    """
-    expected = file_read_table(utila.file_name(source))
-    extracted = tests.caption_.utils.extract_captions(
-        source,
-        ':',
-        testdir,
-        monkeypatch,
-        resultpath=iamraw.path.table_caption,
-        selected='--table',
-    )
-    extracted = utila.flatten(page.content for page in extracted)
-    extracted = [utila.normalize_text(caption.raw) for caption in extracted]
-    extracted: str = utila.NEWLINE.join(extracted)
-    assert extracted == expected
-
 
 BACHELOR51PAGE31 = """\
 Tabelle 2: Korrelation nach Pearson (Kreuzung)
@@ -73,26 +42,6 @@ def test_validate_caption_bachelor56page31(testdir, monkeypatch):
     extracted = [utila.normalize_text(caption.raw) for caption in extracted]
     extracted: str = utila.NEWLINE.join(extracted)
     assert extracted == BACHELOR51PAGE31
-
-
-@pytest.mark.parametrize('source', [
-    pytest.param(power.BACHELOR056_PDF, id='bachelor56'),
-])
-def test_validate_caption_image(source, testdir, monkeypatch):
-    expected = file_read_image(utila.file_name(source))
-    extracted = tests.caption_.utils.extract_captions(
-        source,
-        ':',
-        testdir,
-        monkeypatch,
-        resultpath=iamraw.path.image_caption,
-        selected='--image',
-    )
-    extracted = utila.flatten(page.content for page in extracted)
-    extracted = [utila.normalize_text(caption.raw) for caption in extracted]
-    extracted: str = utila.NEWLINE.join(extracted)
-    utila.log(extracted)
-    assert extracted == expected
 
 
 @pytest.mark.parametrize('source, expected', [
