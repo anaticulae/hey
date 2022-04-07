2aa15f70feb5d149f0dd48a248ba8fb8b45277f6
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-04-07 21:07:49 +0200

    test(refactor): simplify test

--- tests/doctextstyle_/test_features.py
+++ tests/doctextstyle_/test_features.py
@@ -21,7 +21,6 @@ import tests.doctextstyle_.conftest
 def test_cluster_extract_default_textsize(master72_text_flat):
     data = master72_text_flat
     default_text = doctextstyle.features.text(data)
-
     # document text size
     assert default_text[0] == 12.0
 
@@ -33,13 +32,14 @@ def test_cluster_extract_pagenumber(master72_text_flat_small):
 
 
 @pytest.mark.parametrize('source, expected', [
-    pytest.param(power.link(power.MASTER116_PDF), 10.91, id='master116'),
-    pytest.param(power.link(power.MASTER098_PDF), 12.0, id='master98'),
-    pytest.param(power.link(power.MASTER099_PDF), 11.04, id='master99'),
-    pytest.param(power.link(power.BACHELOR111_PDF), 11.96, id='bachelor111'),
+    pytest.param(power.MASTER116_PDF, 10.91, id='master116'),
+    pytest.param(power.MASTER098_PDF, 12.0, id='master98'),
+    pytest.param(power.MASTER099_PDF, 11.04, id='master99'),
+    pytest.param(power.BACHELOR111_PDF, 11.96, id='bachelor111'),
 ])
 @utilatest.longrun
 def test_cluster_extract_textsize(source, expected):
+    source = power.link(source)
     flat = tests.doctextstyle_.conftest.create_matrix(source, pages=None)
     default_text = doctextstyle.features.text(flat)
     # document text size
@@ -56,13 +56,14 @@ def test_cluster_extract_footer_small(master72_text_flat_small):
 
 
 @pytest.mark.parametrize('source, expected', [
-    pytest.param(power.link(power.MASTER116_PDF), None, id='master116'),
-    pytest.param(power.link(power.MASTER098_PDF), 9.0, id='master98'),
-    pytest.param(power.link(power.MASTER099_PDF), 9.0, id='master99'),
-    pytest.param(power.link(power.BACHELOR111_PDF), 9.96, id='bachelor111'),
+    pytest.param(power.MASTER116_PDF, None, id='master116'),
+    pytest.param(power.MASTER098_PDF, 9.0, id='master98'),
+    pytest.param(power.MASTER099_PDF, 9.0, id='master99'),
+    pytest.param(power.BACHELOR111_PDF, 9.96, id='bachelor111'),
 ])
 @utilatest.longrun
 def test_cluster_extract_footnote(source, expected):
+    source = power.link(source)
     flat = tests.doctextstyle_.conftest.create_matrix(source, pages=None)
     footnotes = doctextstyle.features.footnote.footnote(flat)
     if expected is None:
@@ -80,15 +81,16 @@ def test_cluster_extract_paragraph_small(master72_text_flat_small):
 
 
 @pytest.mark.parametrize('source, expected', [
-    pytest.param(power.link(power.MASTER116_PDF), (21, 21), id='master116'),
-    pytest.param(power.link(power.MASTER098_PDF), (41, 41), id='master98'),
-    pytest.param(power.link(power.MASTER099_PDF), (38, 38), id='master99'),
-    pytest.param(power.link(power.BACHELOR111_PDF), (24, 24), id='bachelor111'),
+    pytest.param(power.MASTER116_PDF, (21, 21), id='master116'),
+    pytest.param(power.MASTER098_PDF, (41, 41), id='master98'),
+    pytest.param(power.MASTER099_PDF, (38, 38), id='master99'),
+    pytest.param(power.BACHELOR111_PDF, (24, 24), id='bachelor111'),
 ])
 @utilatest.longrun
 def test_cluster_extract_paragraph_before_and_after(source, expected):
     # TODO: VALIDATE EXPECTED LINE DISTANCE, CURRENTLY THERE ARE NOT
     # CHECKED YET.
+    source = power.link(source)
     flat = tests.doctextstyle_.conftest.create_matrix(source, pages=None)
     paragraph = doctextstyle.features.paragraph.paragraph(flat, digits=0)
     assert paragraph == expected
