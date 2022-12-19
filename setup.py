240652ee77d12d5f07b199213ac04d1667cb46d5
Helmut Konrad Schewe <helmutus@outlook.com>
2022-12-19 21:43:52 +0000

    chore(setup): use quick installer

--- setup.py
+++ setup.py
@@ -8,50 +8,20 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
-import os
-import re
+import utila
 
-import setuptools
-
-ROOT = os.path.abspath(os.path.dirname(__file__))
-
-with open(os.path.join(ROOT, 'README.md'), encoding='utf8') as fp:
-    README = fp.read()
-
-with open(os.path.join(ROOT, 'hey/__init__.py'), encoding='utf8') as fp:
-    VERSION = re.search(r'__version__ = \'(.*?)\'', fp.read()).group(1)
-
-with open(os.path.join(ROOT, 'requirements.txt'), encoding='utf-8') as fp:
-    REQUIRES = [line for line in fp.readlines() if line and '#' not in line]
+PACKAGES = [
+    'doctextstyle',
+    'doctextstyle.features',
+    'doctextstyle.vector',
+    'hey',
+    'magic',
+    'magic.feature',
+]
+ENTRY_POINTS = dict(console_scripts=[
+    'doctextstyle = doctextstyle.cli:main',
+    'magic = magic.cli:main',
+])
 
 if __name__ == "__main__":
-    setuptools.setup(
-        author='Helmut Konrad Fahrendholz',
-        author_email='info@checkitweg.de',
-        description='a lot of features',
-        install_requires=REQUIRES,
-        long_description=README,
-        name='hey',
-        platforms='any',
-        url='https://dev.package.checkitweg.de/hey',
-        version=VERSION,
-        zip_safe=False,  # create 'zip'-file if True. Don't do it!
-        classifiers=[
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8',
-        ],
-        packages=[
-            'doctextstyle',
-            'doctextstyle.features',
-            'doctextstyle.vector',
-            'hey',
-            'magic',
-            'magic.feature',
-        ],
-        entry_points={
-            'console_scripts': [
-                'doctextstyle = doctextstyle.cli:main',
-                'magic = magic.cli:main',
-            ],
-        },
-    )
+    utila.install(__file__)
